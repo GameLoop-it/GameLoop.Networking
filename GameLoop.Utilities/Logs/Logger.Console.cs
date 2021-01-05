3c853c7c88ba8cdfe7a7e752aff4f388a400c769
ManHunterITA <m4nu.91@gmail.com>
2021-01-05 15:05:46 +0000

    update(Logger): improved the Logger utility to include caller's metadata

--- GameLoop.Utilities/Logs/Logger.Console.cs
+++ GameLoop.Utilities/Logs/Logger.Console.cs
@@ -1,3 +1,26 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2020 Emanuele Manzione
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 using System;
 
 namespace GameLoop.Utilities.Logs
@@ -13,38 +36,44 @@ namespace GameLoop.Utilities.Logs
             );
         }
 
-        private static void OnInfoCallbackForConsole(string message)
+        private static void OnInfoCallbackForConsole(LogContext context)
         {
             var previousConsoleColor = Console.ForegroundColor;
 
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("[INF] ");
+            Console.Write(context.Level);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write($"[{context.CallerFile}::{context.CallerMethod}@{context.CallerLine}] ");
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine(message);
+            Console.WriteLine(context.Message);
 
             Console.ForegroundColor = previousConsoleColor;
         }
 
-        private static void OnWarningCallbackForConsole(string message)
+        private static void OnWarningCallbackForConsole(LogContext context)
         {
             var previousConsoleColor = Console.ForegroundColor;
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("[WRN] ");
+            Console.Write(context.Level);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write($"[{context.CallerFile}::{context.CallerMethod}@{context.CallerLine}] ");
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine(message);
+            Console.WriteLine(context.Message);
 
             Console.ForegroundColor = previousConsoleColor;
         }
 
-        private static void OnErrorCallbackForConsole(string message)
+        private static void OnErrorCallbackForConsole(LogContext context)
         {
             var previousConsoleColor = Console.ForegroundColor;
 
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("[ERR] ");
+            Console.Write(context.Level);
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write($"[{context.CallerFile}::{context.CallerMethod}@{context.CallerLine}] ");
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine(message);
+            Console.WriteLine(context.Message);
 
             Console.ForegroundColor = previousConsoleColor;
         }
