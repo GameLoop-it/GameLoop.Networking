fe8088deaa402d21f99a0d9bb7b87366d8e218ed
ManHunterITA <m4nu.91@gmail.com>
2021-01-05 15:12:10 +0000

    feat(Host): added a temporary Host application as sample for testing purposes

---
+++ GameLoop.Networking.Host/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using GameLoop.Utilities.Logs;
+
+namespace GameLoop.Networking.Host
+{
+    public class TestPeer
+    {
+        public const int ServerPort = 25005;
+
+        public  NetworkPeer Peer;
+        private bool        _isServer;
+
+        public static IPEndPoint ServerEndpoint => new IPEndPoint(IPAddress.Loopback, ServerPort);
+
+        public TestPeer(bool isServer)
+        {
+            _isServer = isServer;
+            Peer      = new NetworkPeer(GetNetworkContext(isServer));
+            
+            if (!isServer)
+                Peer.Connect(ServerEndpoint);
+        }
+
+        private static NetworkContext GetNetworkContext(bool isServer)
+        {
+            var context = new NetworkContext();
+            GetNetworkSettings(isServer, context);
+
+            return context;
+        }
+        
+        private static void GetNetworkSettings(bool isServer, NetworkContext context)
+        {
+            if (isServer)
+                context.Settings.BindingEndpoint = ServerEndpoint;
+            else
+                context.Settings.BindingEndpoint = new IPEndPoint(IPAddress.Any, 0);
+        }
+
+        public void Update()
+        {
+            Peer.Update();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.InitializeForConsole();
+
+            var peers = new List<TestPeer>();
+            peers.Add(new TestPeer(true));
+            peers.Add(new TestPeer(false));
+            
+            foreach (var peer in peers)
+            {
+                peer.Peer.Start();
+            }
+
+            while (true)
+            {
+                foreach (var peer in peers)
+                {
+                    peer.Update();
+                }
+                
+                Thread.Sleep(15);
+            }
+        }
+    }
+}
